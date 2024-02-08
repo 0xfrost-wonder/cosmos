3f54c4980c42c750ffd10d5dd724f7dbde870215
COOLICEBLAST <cooliceblast0904@gmail.com>
2024-02-08 23:41:44 +0100

    cosmos

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+// import { StargateClient } from "@cosmjs/stargate";
+
+const App = () =>{
+/*    useEffect(() => {
+        connectStargate();
+      }, []);
+
+
+    const connectStargate = async () => {
+        try {
+            const stargateClient = await StargateClient.connect(
+              'RPC_ENDPOINT_URL_HERE'
+            );
+            setClient(stargateClient);
+            console.log("CosmJS Stargate Connected @",await stargateClient.getChainId());
+          } catch (error) {
+            console.error('Error connecting to Stargate:', error);
+          }
+      };
+*/
+    return (
+        <h1> 
+            <center>The Beginning of a Masterful App!</center>
+        </h1>
+    )
+}
+
+
+export default App
